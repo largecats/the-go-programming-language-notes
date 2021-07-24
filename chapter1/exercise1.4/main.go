4273c404131d5979e01004440aa0de07f4be6d8c
largecats <linfanxiaolinda@outlook.com>
2021-07-24 22:10:12 +0800

    updated

--- chapter1/exercise1.4/main.go
+++ chapter1/exercise1.4/main.go
@@ -8,8 +8,8 @@ import (
 
 func main() {
 	counts := make(map[string]int)
-	appearInFiles := make(map[string][]string)
-	files := os.Args[1:] // list of file names
+	appearInFiles := make(map[string]map[string]bool) // line -> "set" of all files in which the line appears (implemented via map of filename -> true)
+	files := os.Args[1:]                              // list of file names
 	if len(files) == 0 {
 		countLines(os.Stdin, counts, appearInFiles, "stdin") // if no file name is supplied, read from standard input
 	} else {
@@ -25,17 +25,27 @@ func main() {
 	}
 	for line, n := range counts {
 		if n > 1 {
-			fmt.Printf("%d\t%s\t%v\n", n, line, appearInFiles[line])
+			var filenames []string // extract filenames in which line appears
+			for filename := range appearInFiles[line] {
+				filenames = append(filenames, filename)
+			}
+			fmt.Printf("%d\t%s\t%v\n", n, line, filenames)
 		}
 	}
 }
 
-func countLines(f *os.File, counts map[string]int, appearInFiles map[string][]string, filename string) {
+func countLines(f *os.File, counts map[string]int, appearInFiles map[string]map[string]bool, filename string) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
 		line := input.Text()
 		counts[line]++
-		appearInFiles[line] = append(appearInFiles[line], filename)
+		if _, ok := appearInFiles[line]; ok { // if line is already present in appearInFiles
+			if _, ok := appearInFiles[line][filename]; !ok { // add filename if it is not yet recorded
+				appearInFiles[line][filename] = true
+			}
+		} else {
+			appearInFiles[line] = map[string]bool{filename: true}
+		}
 	}
 }
 
@@ -47,5 +57,5 @@ can you hear me
 2       hello   [stdin stdin]
 
 > go run .\main.go sample1.txt sample2.txt
-2       hello   [sample1.txt sample1.txt]
+3       hello   [sample1.txt sample2.txt]
 */
